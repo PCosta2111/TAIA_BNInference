c44e8372017ac7493d03a26b42bb5d10fd3e7123
PCosta2111 <40245028+PCosta2111@users.noreply.github.com>
2020-03-22 14:33:04 +0000

    Added console arguments for max iterations, convergence factor and number of times you want to infer a probability.

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ from Utils import process_arguments, get_value_var, is_in_prob_list
 parser = argparse.ArgumentParser(description='Estimate a probability.')
 parser.add_argument('-t', '--target', metavar='xi')
 parser.add_argument('-u', '--universe', nargs='+')
+parser.add_argument('-i', '--maxiter', metavar='20000', default=20000, type=int)
+parser.add_argument('-c', '--converfactor', metavar='0.001', default=0.001, type=float)
+parser.add_argument('-r', '--repeat', metavar='1', default=1, type=int)
 args = parser.parse_args()
 
 f = open("earthquake.bif", "r")
@@ -15,10 +18,6 @@ BIF = f.readlines()
 BIF = fix_white_space(BIF)
 nodes = parse_bif(BIF)
 t, u = process_arguments(args, nodes)
-
-v = {}
-for n in nodes:
-    v[n] = get_value_var(n, t, u)
 
 
 def initialize_variables():
@@ -40,28 +39,34 @@ def estimate(xi, all_probs):
     return res
 
 
-prob_lst = initialize_variables()
-cols = [n.name for n in nodes]
-df = pd.DataFrame(columns=cols)
-i = 0
-prev_prob = 0
-
-while len(df) < 20000:
-    for xi in nodes:
-        probability = estimate(xi, prob_lst)
-        if not is_in_prob_list(xi, u):
-            r = random.uniform(0, 1)
-            if r > probability:
-                v[xi] = not (v[xi])
+for it in range(0, args.repeat):
+    v = {}
+    for n in nodes:
+        v[n] = get_value_var(n, t, u)
     prob_lst = initialize_variables()
-    i += 1
-    if i > 1000:
-        df.loc[len(df)] = list(v.values())
-    if i % 5000 == 0:
-        curr_prob = len(df[df[t[0].name]]) / len(df)
-        if (abs(curr_prob) - prev_prob) <= 0.001:
-            break
-        prev_prob = curr_prob
+    cols = [n.name for n in nodes]
+    df = pd.DataFrame(columns=cols)
+    i = 0
+    prev_prob = 0
 
-print(df)
-print(len(df[df[t[0].name]]) / len(df))
+    print("Processing inference number : " + str(it))
+    while len(df) < args.maxiter:
+        for xi in nodes:
+            probability = estimate(xi, prob_lst)
+            if not is_in_prob_list(xi, u):
+                r = random.uniform(0, 1)
+                if r > probability:
+                    v[xi] = not (v[xi])
+        prob_lst = initialize_variables()
+        i += 1
+        if i > 1000:
+            df.loc[len(df)] = list(v.values())
+        if i % 5000 == 0:
+            curr_prob = len(df[df[t[0].name]]) / len(df)
+            if (abs(curr_prob) - prev_prob) <= args.converfactor:
+                print("   => Results appear to have converged after " + str(
+                    len(df) + 1000) + " iterations. Result: " + str(len(df[df[t[0].name]]) / len(df)))
+                break
+            prev_prob = curr_prob
+    if len(df) > args.maxiter:
+        print("   => Maximum number of iterations reached. Result: " + str(len(df[df[t[0].name]]) / len(df)))
